755cae2ec0a43e5202d8d78092fe21a4553aca61
wabish0 <francismateo444@gmail.com>
2025-06-12 23:52:00 -0400

    Agregar archivos de proyecto.

---
+++ Simulacion/MomoProcesador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Monoprocesador
+{
+    private List<Tarea> cola_tareas;
+
+    public Monoprocesador()
+    {
+        cola_tareas = new List<Tarea>();
+    }
+
+    public void AgregarTarea(Tarea tarea)
+    {
+        cola_tareas.Add(tarea);
+    }
+
+    public void EjecutarTareas()
+    {
+        Console.WriteLine("\n== Iniciando ejecución de tareas ==\n");
+
+        foreach (Tarea tarea in cola_tareas)
+        {
+            tarea.Ejecutar();
+        }
+
+        Console.WriteLine("== Todas las tareas han sido completadas ==");
+    }
+}
---
+++ Simulacion/Tarea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+public class Tarea
+{
+    public string Nombre { get; set; }
+    public int Duracion { get; set; } 
+
+    public Tarea(string nombre, int duracion)
+    {
+        Nombre = nombre;
+        Duracion = duracion;
+    }
+
+    public void Ejecutar()
+    {
+        Console.WriteLine($"Iniciando tarea: {Nombre}");
+        Thread.Sleep(Duracion * 1000);
+        Console.WriteLine($"Tarea finalizada: {Nombre}\n");
+    }
+}
